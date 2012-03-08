7d46d4060d33f98329104fd4a224efbc4c9a66b2
Kevin Barnes <kbarnes3@gmail.com>
2012-03-08 01:26:53 -0800

    More settings work

---
+++ Bijk-Viewer/Bijk-Viewer/js/accountSettingsFlyout.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    "use strict";
+    var applicationData = Windows.Storage.ApplicationData.current;
+
+    var page = WinJS.UI.Pages.define("/html/accountSettingsFlyout.html", {
+        ready: function (element, options) {
+            var apiKeyInput = document.getElementById("apiKeyInput");
+            apiKeyInput.addEventListener("blur", apiKeyInputBlur, false);
+
+            if ("bijkApiKey" in applicationData.roamingSettings) {
+                apiKeyInput.value = "Hi";
+            }
+            else {
+                apiKeyInput.value = "Low";
+            }
+
+            window.addEventListener("beforeunload", apiKeyInputBlur);
+        }
+    });
+
+    function apiKeyInputBlur() {
+        debugger;
+    }
+})();
